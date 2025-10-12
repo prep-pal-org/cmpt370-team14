8779ae74de72b7e647331734a68e4d088a5887d8
Jordan Mueller <jpm175@usask.ca>
2025-10-11 20:26:46 -0600

    setup_database.py re-organized to be more modular for ease of adding new tables. Added error checking and documentation throughout. Added two calendar based tables and tested script which seems to generate as expected.

--- db/setup_database.py
+++ db/setup_database.py
@@ -1,30 +1,107 @@
 import sqlite3
+from _sqlite3 import Error
 
-#setup common website database
-def setup_database():
-    #Connect to SQLite database file
-    connection = sqlite3.connect('saucyapp.db')
+"""
+database_connection function - used to create a connection to a SQLite database
+Pre-Conditions:
+    database - a relative filepath to a .db file
+Post-Conditions:
+    return reference to the database connection
+"""
+def database_connection(database):
+    connection = None
+    try:
+        #Connect to SQLite database file
+        connection = sqlite3.connect(database)
+    except Error as e:
+        print('Error while connecting to database', e)
+    return connection
 
-    #Create cursor for database operations
-    cursor = connection.cursor()
+"""
+create_tables function - used to create the tables in the database
+Pre-Conditions:
+    connection - an established connection to a SQLite database
+Post-Conditions:
+    return True if no Errors occurred
+    return False if Errors encountered
+"""
+def create_tables(connection):
+    try:
+        #Create cursor for database operations
+        cursor = connection.cursor()
 
-    #Create Calendar Event Table
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS calendar_events (
+        #calendar_event table - Jordan
+        create_calendar_event = '''
+        CREATE TABLE IF NOT EXISTS calendar_event (
             event_id INTEGER PRIMARY KEY AUTOINCREMENT,
             event_name TEXT,
             event_date DATE,
-        );    
-    ''')
+            event_time TEXT,
+            recipe_id INTEGER,
+            calendar_id INTEGER,
+            recurrence_id INTEGER,
+            FOREIGN KEY(calendar_id) REFERENCES calendar_schedule (calendar_id),
+            FOREIGN KEY(recurrence_id) REFERENCES recurring_event (recurrence_id),
+            CONSTRAINT unique_event UNIQUE(event_date, event_time)                
+        );
+        '''
+
+        #calendar_schedule table - Jordan
+        create_calendar_schedule = '''
+        CREATE TABLE IF NOT EXISTS calendar_schedule (
+            calendar_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            event_id INTEGER,
+            user_id INTEGER,
+            FOREIGN KEY(event_id) REFERENCES calendar_event (event_id)        
+        );
+        '''
+
+        #recurring calendar event table - Jordan
 
 
-    #Create Other Table
 
-    #Create Other Table
+        #Other tables here \/\/\/
+        #TODO - add all other tables
 
-    connection.commit()
-    connection.close()
+
+
+
+        #Create List of all table creation text
+        #TODO - add tables created to this list
+        table_list = [create_calendar_event, create_calendar_schedule]
+
+        #Loop through list for execute, actually creating tables
+        for table in table_list:
+            cursor.execute(table)
+
+        #Create Indexes? for easier/quicker searching? not sure if needed.
+        #cursor.execute('CREATE INDEX IF NOT EXISTS schedule_user_id ON calendar_schedule (user_id);')
+
+    #If error encountered, print error, return false
+    except Error as e:
+        print('Error while creating tables', e)
+        return False
+    #successful table created, return true
+    return True
+
+
+
+def main():
+    #Connect to database
+    database = 'saucyapp.db'
+    connection = database_connection(database)
+
+    #Check connection established
+    if connection is not None:
+        #If successful, try to create tables
+        if create_tables(connection):
+            print('Tables created')
+        else:
+            print('Tables not created')
+    else:
+        print('Database connection failed')
+
+
 
 if __name__ == '__main__':
-    #setup_database()
-    print('Setting up database...')
+    main()
