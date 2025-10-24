938427eba32b5927e411228e8dc8bd07e0977e02
Randi Scansen <tqh789@usask.ca>
2025-10-24 17:00:56 -0600

    Randi - the static folder containing the javascript event handler and css style design page.

---
+++ cmpt370_project_user_management/Static/eventHandling.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const button = document.getElementById('CreateProfile')
+
+    button.addEventListener('click',function(){
+        alert("Button was clicked!")
+    });
+});
