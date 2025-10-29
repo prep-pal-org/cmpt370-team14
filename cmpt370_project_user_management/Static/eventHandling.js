ff04c28cf221ecc0a430c3ab2d9ee07648c48faa
Randi Scansen <tqh789@usask.ca>
2025-10-29 11:00:24 -0600

    Randi - Updated with add comment and view comments buttons

--- cmpt370_project_user_management/Static/eventHandling.js
+++ cmpt370_project_user_management/Static/eventHandling.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     const button1 = document.getElementById('CreateProfile')
     const button2 = document.getElementById('SeeUsers')
+    const button3 = document.getElementById('comment')
+    const button4 = document.getElementById('viewcomment')
 
     button1.addEventListener('click',function() {
         window.location.href = "/create_profile";
@@ -9,6 +11,14 @@ document.addEventListener('DOMContentLoaded', function(){
 
     button2.addEventListener('click',function() {
         window.location.href = "/user_list_for_testing";
+    });
+
+    button3.addEventListener('click',function(){
+        window.location.href = "/create_comment";
+    });
+
+    button4.addEventListener('click', function (){
+        window.location.href = "/view_comments";
     });
 
 });
