a35c1362ac51eb4be0c508c37dfed93a8c2d45e8
Randi Scansen <tqh789@usask.ca>
2025-10-29 10:59:51 -0600

    Randi- MealPlanner.py updated to include create_comment.html and view_comments.html

--- cmpt370_project_user_management/MealPlanner.py
+++ cmpt370_project_user_management/MealPlanner.py
@@ -31,5 +31,28 @@ def user_list_for_testing():
     rows = cur.fetchall()
     return render_template("user_list_for_testing.html", data=rows)
 
+@app.route('/create_comment',methods=['GET', 'POST'])
+def create_comment():
+    if request.method == 'POST':
+        # add a line to get the name of the commenter
+        comment = request.form['comment']
+        with sqlite3.connect('db/saucyapp.db') as conn:
+            cur = conn.cursor()
+            cur.execute(" INSERT INTO user_interaction (comment) VALUES (?)",
+                        (comment,))
+            conn.commit()
+        return render_template('homePage.html')
+    else:
+        return render_template('create_comment.html')
+
+@app.route('/view_comments')
+def view_comment():
+    connect = sqlite3.connect('db/saucyapp.db')
+    cur = connect.cursor()
+    cur.execute(" SELECT * FROM user_interaction")
+    com_rows = cur.fetchall()
+    return render_template("view_comments.html", com_data=com_rows)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
