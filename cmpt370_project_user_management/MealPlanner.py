39d42682af7ae4791935f189a99ff3917280b701
Baraa <vea470@usask.ca>
2025-11-02 16:28:35 -0600

    update mealplayyer with recipe routes- add recipe and delete recipe

--- cmpt370_project_user_management/MealPlanner.py
+++ cmpt370_project_user_management/MealPlanner.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from Controller.RecipeManager import RecipeManager
+from Model.Recipe import Recipe
 import sqlite3
 
 app = Flask(__name__)
+manager = RecipeManager()
+
 
 # Routing to a home page.
 @app.route('/')
@@ -30,6 +34,47 @@ def user_list_for_testing():
     cur.execute(" SELECT * FROM user_profile")
     rows = cur.fetchall()
     return render_template("user_list_for_testing.html", data=rows)
+
+# --------------------------------------------------------
+# ROUTE: Show all recipes
+# Baraa
+# --------------------------------------------------------
+@app.route('/recipes')
+def recipe_list():
+    """Display all recipes from the database."""
+    recipes = manager.getAllRecipes()
+    return render_template('recipe_list.html', recipes=recipes)
+
+
+# --------------------------------------------------------
+# ROUTE: Add a new recipe
+# Baraa
+# --------------------------------------------------------
+@app.route('/recipes/add', methods=['GET', 'POST'])
+def add_recipe():
+    """Handle the form to add a new recipe."""
+    if request.method == 'POST':
+        name = request.form['name']
+        ingredients = request.form['ingredients']
+        instructions = request.form['instructions']
+
+        new_recipe = Recipe(name=name, ingredients=ingredients, instructions=instructions)
+        manager.addRecipe(new_recipe)
+
+        return redirect(url_for('recipe_list'))
+    return render_template('recipe_add.html')
+
+
+# --------------------------------------------------------
+# ROUTE: Delete a recipe
+# Baraa
+# --------------------------------------------------------
+@app.route('/recipes/delete/<int:recipe_id>', methods=['POST'])
+def delete_recipe(recipe_id):
+    """Delete a recipe by its ID."""
+    manager.deleteRecipe(recipe_id)
+    return redirect(url_for('recipe_list'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
