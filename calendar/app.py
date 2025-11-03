e023d2350067913fefef2c1e3e4cd8bcc36bdc8f
Jordan Mueller <jpm175@usask.ca>
2025-11-02 18:44:43 -0600

    Calendar Feature - Created Flask starter script app.py with routes for a basic index page to obtain user_id to be replaced with user management front end, a calender view page and api routes for populating the calendar, adding event and deleting event. HTML files created for base page and extended index and calendar. Next to look at JS event handling for the calender and CSS styling. Recipe ID still being entered manually, to be integrated with Search and Recipe systems

---
+++ calendar/app.py
@@ -0,0 +1,86 @@
+from calendar_service import*
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+
+app = Flask(__name__)
+service = CalendarService()
+
+#Basic temporary index before integrating with user login function
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        #proceeding with user_id
+        session["user_id"] = request.form.get("user_id")
+        return redirect(url_for("calendar_view"))
+    else:
+        return render_template("index.html")
+
+#Calendar View route
+@app.route('/calendar')
+def calendar_view():
+    if "user_id" in session:
+        return redirect(url_for("index"))
+    else:
+        return render_template("calendar.html")
+
+#Helper method to get calendar id
+def get_calendar():
+    user_id = session.get("user_id")
+    if user_id is None:
+        return None
+    calendar_id = service.create_new_calendar(user_id)
+    return calendar_id
+
+
+#API - Get Events for current calendar
+@app.route("/api/events")
+def api_events():
+
+
+    #Get calendar_id for user_id, then get corresponding calendar events
+    calendar_id = get_calendar()
+    if calendar_id is None:
+        return jsonify([]) #no events for no user_id
+    events = service.get_calendar_events(calendar_id)
+
+    #Convert to FullCalendar JSON format
+    full_calendar_events = []
+    for event in events:
+        full_calendar_events.append({
+            "id": event.getEventID(),
+            "title": event.getEventName(),
+            "start": event.getEventDate(),
+            "extendedProps":{
+                "timeSlot": event.getEventTime()
+            }
+        })
+    return jsonify(full_calendar_events)
+
+
+#API - Add new event
+@app.route("/api/events", methods=["POST"])
+def api_add_event():
+    data = request.json
+    calendar_id = get_calendar()
+    if calendar_id is None:
+        return jsonify({"error": "no calendar id"}), 400
+
+    #Create event using calendar service
+    event_id = service.insert_calendar_event(
+        recipe_id=data["recipe_id"],
+        calendar_id=calendar_id,
+        event_name=data["event_name"],
+        event_date=data["event_date"],
+        event_time=data["event_time"]
+    )
+    return jsonify({"event_id": event_id})
+
+#API - delete existing event
+@app.route("/api/events/<int:event_id>", methods=["DELETE"])
+def api_delete_event(event_id):
+    deleted_event = service.delete_calendar_event(event_id)
+    if deleted_event is None:
+        return jsonify({"event deleted": False}), 404
+    return jsonify({"event deleted": True})
+
+if __name__ == "__main__":
+    app.run(debug=True)
