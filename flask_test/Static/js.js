5f4a5fd30948f8c61ae63c4cbbcbdce268c66668
Baraa <vea470@usask.ca>
2025-11-27 19:27:56 -0600

    UI updates + landing page routing + recipe protections + updated styles

--- flask_test/Static/js.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('showTableButton');
-    const table = document.getElementById('myTable');
-
-    button.addEventListener('click', function() {
-        table.style.display = 'table';
-    });
-});
